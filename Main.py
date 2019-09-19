42c35e045afbc557ee77aa7a9fad6ab180fec9b3
LukasJidell <55480880+LukasJidell@users.noreply.github.com>
2019-09-19 13:43:34 +0200

    Add files via upload
    Code that does nothing, with a few changes it will count words in the example data.

---
+++ Main.py
@@ -0,0 +1,144 @@
+import string
+from collections import Counter
+from os import listdir
+from string import punctuation
+import keras
+
+# load doc into memory
+from nltk.corpus import stopwords
+def load_doc(filename):
+	# open the file as read only
+	file = open(filename, 'r')
+	# read all text
+	text = file.read()
+	# close the file
+	file.close()
+	return text
+
+# turn a doc into clean tokens
+""" Denna ska vara bättre men den funkar inte. 
+def clean_doc(doc):
+	# split into tokens by white space
+	tokens = doc.split()
+	# remove punctuation from each token
+	table = str.maketrans('', '', punctuation)
+	tokens = [w.translate(table) for w in tokens]
+	# remove remaining tokens that are not alphabetic
+	tokens = [word for word in tokens if word.isalpha()]
+	# filter out stop words
+	stop_words = set(stopwords.words('english'))
+	tokens = [w for w in tokens if not w in stop_words]
+	# filter out short tokens
+	tokens = [word for word in tokens if len(word) > 1]
+	return tokens
+"""
+
+# turn a doc into clean tokens
+def clean_doc(doc, vocab):
+	# split into tokens by white space
+	tokens = doc.split()
+	# remove punctuation from each token
+	table = str.maketrans('', '', punctuation)
+	tokens = [w.translate(table) for w in tokens]
+	# filter out tokens not in vocab
+	tokens = [w for w in tokens if w in vocab]
+	tokens = ' '.join(tokens)
+	return tokens
+
+# load doc and add to vocab
+def add_doc_to_vocab(filename, vocab):
+	# load doc
+	doc = load_doc(filename)
+	# clean doc
+	tokens = clean_doc(doc)
+	# update counts
+	vocab.update(tokens)
+
+# load all docs in a directory
+
+def process_docs(directory, vocab, is_trian):
+	documents = list()
+	# walk through all files in the folder
+	for filename in listdir(directory):
+		# skip any reviews in the test set
+		if is_trian and filename.startswith('cv9'):
+			continue
+		if not is_trian and not filename.startswith('cv9'):
+			continue
+		# create the full path of the file to open
+		path = directory + '/' + filename
+		# load the doc
+		doc = load_doc(path)
+		# clean doc
+		tokens = clean_doc(doc, vocab)
+		# add to list
+		documents.append(tokens)
+	return documents
+
+"""
+# load all docs in a directory
+def process_docs(directory, vocab, is_trian):
+    documents = list()
+    # walk through all files in the folder
+    for filename in listdir(directory):
+        # skip any reviews in the test set
+        if is_trian and filename.startswith('cv9'):
+            continue
+        if not is_trian and not filename.startswith('cv9'):
+            continue
+        # create the full path of the file to open
+        path = directory + '/' + filename
+        # load the doc
+        doc = load_doc(path)
+        # clean doc
+        tokens = clean_doc(doc, vocab)
+        # add to list
+        documents.append(tokens)
+    return documents
+"""
+
+# save list to file
+def save_list(lines, filename):
+    # convert lines to a single blob of text
+    data = '\n'.join(lines)
+    # open file
+    file = open(filename, 'w')
+    # write text
+    file.write(data)
+    # close file
+    file.close()
+
+vocab = Counter()
+positive_docs = process_docs('txt_sentoken/pos', vocab, True)
+negative_docs = process_docs('txt_sentoken/neg', vocab, True)
+train_docs = negative_docs + positive_docs
+print(len(vocab))
+print(vocab.most_common(50))
+
+
+"""
+# define vocab
+vocab = Counter()
+# add all docs to vocab
+process_docs('txt_sentoken/neg', vocab, True)
+process_docs('txt_sentoken/pos', vocab, True)
+# print the size of the vocab
+print(len(vocab))
+# print the top words in the vocab
+print(vocab.most_common(50))
+
+min_occurane = 2
+tokens = [k for k,c in vocab.items() if c >= min_occurane]
+print(len(tokens))
+save_list(tokens, 'vocab.txt')
+
+tokenizer = Tokenizer()
+# fit the tokenizer on the documents
+tokenizer.fit_on_texts(train_docs)
+
+# keep tokens with a min occurrence
+min_occurane = 2
+tokens = [k for k,c in vocab.items() if c >= min_occurane]
+print(len(tokens))
+
+"""
